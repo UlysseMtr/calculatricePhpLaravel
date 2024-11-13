b2be96ad0d626ba9ac766df8a45ed1d5cf77050e
Ulysse <ulyssemetra@gmail.com>
2024-11-13 00:54:10 -0800

    CalculatriceController opération basique faite, utilisation de switch case

--- app/Http/Controllers/CalculatriceController.php
+++ app/Http/Controllers/CalculatriceController.php
@@ -13,7 +13,28 @@ class CalculatriceController extends Controller
 
     public function calcul(Request $request)
     {
-        return view('calculatrice');
+        $num1 = $request->input('num1');
+        $num2 = $request->input('num2');
+        $operateur = $request->input('operande');
+
+        switch ($operateur) {
+            case '+':
+                $resultat = $num1 + $num2;
+                break;
+            case '-':
+                $resultat = $num1 - $num2;
+                break;
+            case '*':
+                $resultat = $num1 * $num2;
+                break;
+            case '/':
+                $resultat = $num1 / $num2;
+                break;
+            default:
+                $resultat = 'Opérateur non valide';
+        }
+
+        return view('calculatrice', ['resultat' => $resultat, 'num1' => $num1, 'num2' => $num2, 'operateur' => $operateur]);
     }
 
 }
