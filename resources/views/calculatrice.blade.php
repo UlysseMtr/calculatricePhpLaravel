cbfe343317adae907221d32fa2dd6f6f6482cb02
Ulysse <ulyssemetra@gmail.com>
2024-11-13 00:34:31 -0800

    Premier commit initialisation laravel + creation CalculatriceController + et fichier blade

---
+++ resources/views/calculatrice.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device=width, initial-scale1.0">
+        <title>Calculatrice</title>
+    </head>
+    <body>
+        <h1>Calculez vos opérations ici</h1>
+    </body>
+
+</html>
